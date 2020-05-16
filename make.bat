b89a03c397e36c41a789e972b59ad286c089c9f5
Rokett <rokett@rokett.me>
2020-05-16 10:00:47 +0100

    Migrated from dep to Go modules

--- make.bat
+++ make.bat
@@ -1,8 +1,6 @@
 @echo off
 SETLOCAL
 
-set _TARGETS=build
-
 set APP=Alertmanager-Forwarder
 set VERSION=1.1.0
 set BINARY-WINDOWS-X64=%APP%_%VERSION%_Windows_amd64.exe
@@ -11,24 +9,22 @@ set BINARY-LINUX=%APP%_%VERSION%_Linux_amd64
 REM Set build number from git commit hash
 for /f %%i in ('git rev-parse HEAD') do set BUILD=%%i
 
-if [%1]==[] goto build
-
 set LDFLAGS=-ldflags "-X main.version=%VERSION% -X main.build=%BUILD% -s -w"
 
-goto %1
+goto build
 
 :build
     echo "=== Building Windows x64 ==="
     set GOOS=windows
     set GOARCH=amd64
 
-    go build -o %BINARY-WINDOWS-X64% %LDFLAGS%
+    go build -mod=vendor -o %BINARY-WINDOWS-X64% %LDFLAGS%
 
     echo "=== Building Linux x64 ==="
     set GOOS=linux
     set GOARCH=amd64
 
-    go build -o %BINARY-LINUX% %LDFLAGS%
+    go build -mod=vendor -o %BINARY-LINUX% %LDFLAGS%
 
     goto :finalise
 
